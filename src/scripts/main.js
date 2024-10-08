2bd2b843accc4e168c34615691366dfcc037d02a
mason870 <mason.cayer@innovationcharter.org>
2024-10-08 14:57:43 +0000

    step 5 mason

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,12 @@
 import "../styles/styles.css";
 // Add imports here
-
+import Aos from "aos"; 
+import "aos/dist/aos.css";
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
   // Put code that runs ONLY in the browser here -- this is most likely where
+  Aos.init();
   // MOST of your code should go.
   //
   // If you see a SSR error, try moving your code here!
